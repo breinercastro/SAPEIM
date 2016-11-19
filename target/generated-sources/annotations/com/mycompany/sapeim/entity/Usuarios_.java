b12155bdd407a34ff8c6051aa35b93e173816230
breinercastro <jbreiner.castro@yopmail.com>
2016-11-18 23:02:10 -0500

    15/08/2016 15:41 prueba final

--- target/generated-sources/annotations/com/mycompany/sapeim/entity/Usuarios_.java
+++ target/generated-sources/annotations/com/mycompany/sapeim/entity/Usuarios_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-11-18T11:22:20")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-11-18T22:16:51")
 @StaticMetamodel(Usuarios.class)
 public class Usuarios_ { 
 
