34120813beca0f351fee0b4ecd3b390bfd02a328
breinercastro <jbreiner.castro@yopmail.com>
2016-11-20 12:24:56 -0500

    2 modificacion al primeface

--- target/generated-sources/annotations/com/mycompany/sapeim/entity/Equipos_.java
+++ target/generated-sources/annotations/com/mycompany/sapeim/entity/Equipos_.java
@@ -11,7 +11,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-11-20T11:16:03")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-11-18T22:16:51")
 @StaticMetamodel(Equipos.class)
 public class Equipos_ { 
 
