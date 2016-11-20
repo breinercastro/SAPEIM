555fb62cc05f78dd229f6d59919121e0b93673d1
breinercastro <jbreiner.castro@yopmail.com>
2016-11-20 11:26:31 -0500

    15/08/2016 15:41 Agrege primeface

--- target/generated-sources/annotations/com/mycompany/sapeim/entity/EstadoEquipo_.java
+++ target/generated-sources/annotations/com/mycompany/sapeim/entity/EstadoEquipo_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-11-18T22:16:51")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-11-20T11:16:03")
 @StaticMetamodel(EstadoEquipo.class)
 public class EstadoEquipo_ { 
 
